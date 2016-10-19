392b0ae39b6e17d83f217ce490aa148c588ac0b5
Lazaridis <jordanis.lazaridis@haw-hamburg.de>
2016-10-19 15:06:05 +0200

    leeres_Fenster

---
+++ ImageEditorFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*; 
+import java.awt.event.*;
+import java.awt.*; 
+import java.awt.image.*; 
+import java.io.*; 
+import javax.imageio.*; 
+ 
+public class ImageEditorFrame extends JFrame{   
+ public ImageEditorFrame(){       
+ 
+ setDefaultCloseOperation(EXIT_ON_CLOSE);     
+ setSize(400,  300);      
+ setVisible(true);    
+ }
+
+public static void main (String [] args){
+	new ImageEditorFrame();
+	
+} 
+}
